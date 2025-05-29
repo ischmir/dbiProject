02d95efbdda5ec5e85fd1ce210c8bc9bee1e3203
Mai Jockwich <mmjockwich@gmail.com>
2025-05-29 19:52:20 +0200

    chore: linting

--- jest.config.mjs
+++ jest.config.mjs
@@ -22,7 +22,7 @@ const config = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    'src/**/*.js'
+    'src/**/*.js',
   ],
 
   // The directory where Jest should output its coverage files
