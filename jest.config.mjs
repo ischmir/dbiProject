ef3be2b4f63bf80bce0358407f7fed5f9795562a
Mai Jockwich <mmjockwich@gmail.com>
2025-05-29 19:35:53 +0200

    chore: enable json-summary coverage reporter

--- jest.config.mjs
+++ jest.config.mjs
@@ -35,12 +35,13 @@ const config = {
   // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    // 'json',
+    'json-summary',
+    // "text",
+    // "lcov",
+    // "clover"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
